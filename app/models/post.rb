81defde427b8897985409764d934a70cd5184e8a
vagrant <vagrant@localhost.localdomain>
2020-10-29 02:56:41 +0000

    seed追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,6 @@ class Post < ApplicationRecord
   belongs_to :member
   has_many :pictures, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  accepts_attachments_for :pictures, attachment: :picture
 end
