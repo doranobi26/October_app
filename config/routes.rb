6d331814ebf95eb8c82259351864fa565ff89fd4
vagrant <vagrant@localhost.localdomain>
2020-10-31 07:56:08 +0000

    いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :posts do
     resources :comments, only:[:create, :destroy]
+    resource :nices, only: [:create, :destroy]
   end
 
 
